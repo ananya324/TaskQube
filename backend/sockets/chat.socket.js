299021905409cdb038440bbc6a9ad9522290e2ae
Anya <sanodiaananya@gmail.com>
2026-05-29 16:33:13 +0530

    chat.socket.js

--- backend/sockets/chat.socket.js
+++ backend/sockets/chat.socket.js
@@ -2,6 +2,7 @@ const registerChatSocket = (io, socket) => {
   socket.on("send-message", (message) => {
     io.to(message.workspace).emit("receive-message", message);
   });
+  
 };
 
 module.exports = registerChatSocket;
